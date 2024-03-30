0bddee9455b2e374fff8d441968d43fcaccf9ff7
Myo Thiha Kyaw <myothihakyaw571@gmail.com>
2024-03-30 20:39:49 +0630

    update home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    
+    extend: {
+      fontSize: {
+        '2xl': ['1.5rem', {
+          lineHeight: '2rem',
+          letterSpacing: '-0.01em',
+          fontWeight: '500',
+        }],
+        '3xl': ['1.875rem', {
+          lineHeight: '2.25rem',
+          letterSpacing: '-0.02em',
+          fontWeight: '700',
+        }],
+      }
+    },
   },
   plugins: [],
 }
