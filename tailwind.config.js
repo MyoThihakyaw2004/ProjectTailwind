670614da7c7b031e47c71b5a3c66ec43404b1f99
Jay Mon <kyawgii035@gmail.com>
2024-04-03 09:05:46 +0630

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'noto-sans': ['Noto Sans', 'sans-serif'],
+      },
+      fontWeight: {
+        'custom-weight': '600',
+      },
+      fontVariationSettings: {
+        'custom-settings': '"wdth" 100',
+      },
+    },
   },
   plugins: [],
 }
-
