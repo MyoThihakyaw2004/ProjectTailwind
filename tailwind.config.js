8d1af7325d4074179ae23ec91540e4065ef4a586
Jay Mon <kyawgii035@gmail.com>
2024-04-03 10:51:15 +0630

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-
-    
     extend: {
-      fontSize: {
-        '2xl': ['1.5rem', {
-          lineHeight: '2rem',
-          letterSpacing: '-0.01em',
-          fontWeight: '500',
-        }],
-        '3xl': ['1.875rem', {
-          lineHeight: '2.25rem',
-          letterSpacing: '-0.02em',
-          fontWeight: '700',
-        }],
-      }
->>>>>>> 0bddee9455b2e374fff8d441968d43fcaccf9ff7
+      fontFamily: {
+        'noto-sans': ['Noto Sans', 'sans-serif'],
+      },
+      fontWeight: {
+        'custom-weight': '600',
+      },
+      fontVariationSettings: {
+        'custom-settings': '"wdth" 100',
+      },
     },
   },
   plugins: [],
